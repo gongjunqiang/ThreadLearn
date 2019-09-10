58a31b963606af378c08b9743f6770a0fce0da40
龚均强 <1208754526@qq.com>
2019-09-10 22:19:57 +0800

    111

---
+++ ThreadDemo/Linq/ExtendMethod.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq
+{
+    static class ExtendMethod
+    {
+        public static int Square(this int num)
+        {
+            return num * num;
+        }
+    }
+}
---
+++ ThreadDemo/Linq/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = string.Empty;
+            int a = 10;
+            Console.WriteLine("10的平方是："+a.Square());
+                
+
+            Console.ReadLine();
+        }
+    }
+}
